3ff7eb2f781e7319150f19f9668fbe766649a302
merdaalcanto <merdaalcanto@hotmail.com>
2021-01-19 15:40:27 +0100

    initial commit. Blank project

---
+++ src/main/java/com/M15Dicegame/M15JocDausApplication.java
@@ -0,0 +1,13 @@
+package com.M15Dicegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M15JocDausApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M15JocDausApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/M15Dicegame/M15JocDausApplicationTests.java
@@ -0,0 +1,13 @@
+package com.M15Dicegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M15JocDausApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
